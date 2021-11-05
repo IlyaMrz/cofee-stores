652f240134ced6a42ca299459219e88c5f120740
morozov ilya <knpkaa@gmail.com>
2021-11-05 04:35:36 +0300

    store page styling

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -20,12 +20,7 @@ export async function getStaticProps(staticProps) {
         },
     };
 }
-export async function getStaticPaths() {
-    return {
-        paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-        fallback: true,
-    };
-}
+
 // import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 // import { StoreContext } from "../../store/store-context";
@@ -46,20 +41,20 @@ export async function getStaticPaths() {
 //   };
 // }
 
-// export async function getStaticPaths() {
-//   const coffeeStores = await fetchCoffeeStores();
-//   const paths = coffeeStores.map((coffeeStore) => {
-//     return {
-//       params: {
-//         id: coffeeStore.id.toString(),
-//       },
-//     };
-//   });
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// }
+export async function getStaticPaths() {
+    // const coffeeStores = await fetchCoffeeStores();
+    const paths = coffeStoresData.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString(),
+            },
+        };
+    });
+    return {
+        paths,
+        fallback: true,
+    };
+}
 
 const CoffeeStore = (props) => {
     const router = useRouter();
@@ -119,7 +114,7 @@ const CoffeeStore = (props) => {
 
     // const { address, name, neighbourhood, imgUrl } = coffeeStore;
 
-    // const [votingCount, setVotingCount] = useState(0);
+    const [votingCount, setVotingCount] = useState(0);
 
     // const { data, error } = useSWR(`/api/getCoffeeStoreById?id=${id}`);
 
@@ -157,83 +152,73 @@ const CoffeeStore = (props) => {
     // if (error) {
     //   return <div>Something went wrong retrieving coffee store page</div>;
     // }
-    const { name, address, neighbourhood } = props.coffeeStore;
+    const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
     return (
-        <div>
+        <div className={styles.layout}>
             <Head>
                 <title>{name}</title>
+                <meta name="description" content={`${name} coffee store`}></meta>
             </Head>
-            <Link href="/">back to home</Link>
-            <div> {router.query.id}</div>
-            <div>name {name}</div>
-            <div>address {address}</div>
-            <div>neighbourhood {neighbourhood}</div>
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="/">
+                            <a>← Back to home</a>
+                        </Link>
+                    </div>
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+                    <Image
+                        alt="banner image"
+                        src={
+                            imgUrl ||
+                            "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                        }
+                        width={600}
+                        height={360}
+                        className={styles.storeImg}
+                        alt={name}
+                    ></Image>
+                </div>
+
+                <div className={cls("glass", styles.col2)}>
+                    <div className={styles.iconWrapper}>
+                        <Image
+                            src="/static/icons/places.svg"
+                            width="24"
+                            height="24"
+                            alt="places icon"
+                        />
+                        <p className={styles.text}>{address}</p>
+                    </div>
+                    {neighbourhood && (
+                        <div className={styles.iconWrapper}>
+                            <Image
+                                src="/static/icons/nearMe.svg"
+                                width="24"
+                                height="24"
+                                alt="near me icon"
+                            />
+                            <p className={styles.text}>{neighbourhood}</p>
+                        </div>
+                    )}
+                    <div className={styles.iconWrapper}>
+                        <Image
+                            src="/static/icons/star.svg"
+                            width="24"
+                            height="24"
+                            alt="star icon"
+                        />
+                        <p className={styles.text}>{votingCount}</p>
+                    </div>
+                    {/* 
+                    <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+                        Up vote!
+                    </button> */}
+                </div>
+            </div>
         </div>
-        // <div className={styles.layout}>
-        //     <Head>
-        //         <title>{name}</title>
-        //         <meta name="description" content={`${name} coffee store`}></meta>
-        //     </Head>
-        //     <div className={styles.container}>
-        //         <div className={styles.col1}>
-        //             <div className={styles.backToHomeLink}>
-        //                 <Link href="/">
-        //                     <a>← Back to home</a>
-        //                 </Link>
-        //             </div>
-        //             <div className={styles.nameWrapper}>
-        //                 <h1 className={styles.name}>{name}</h1>
-        //             </div>
-        //             <Image
-        //                 alt="banner image"
-        //                 src={
-        //                     imgUrl ||
-        //                     "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-        //                 }
-        //                 width={600}
-        //                 height={360}
-        //                 className={styles.storeImg}
-        //                 alt={name}
-        //             ></Image>
-        //         </div>
-
-        //         <div className={cls("glass", styles.col2)}>
-        //             <div className={styles.iconWrapper}>
-        //                 <Image
-        //                     src="/static/icons/places.svg"
-        //                     width="24"
-        //                     height="24"
-        //                     alt="places icon"
-        //                 />
-        //                 <p className={styles.text}>{address}</p>
-        //             </div>
-        //             {neighbourhood && (
-        //                 <div className={styles.iconWrapper}>
-        //                     <Image
-        //                         src="/static/icons/nearMe.svg"
-        //                         width="24"
-        //                         height="24"
-        //                         alt="near me icon"
-        //                     />
-        //                     <p className={styles.text}>{neighbourhood}</p>
-        //                 </div>
-        //             )}
-        //             <div className={styles.iconWrapper}>
-        //                 <Image
-        //                     src="/static/icons/star.svg"
-        //                     width="24"
-        //                     height="24"
-        //                     alt="star icon"
-        //                 />
-        //                 <p className={styles.text}>{votingCount}</p>
-        //             </div>
-
-        //             <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
-        //                 Up vote!
-        //             </button>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
