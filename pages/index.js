710a8a2d8e3beaeb3cde3159df99e1320bed85e8
morozov ilya <knpkaa@gmail.com>
2021-11-05 04:21:26 +0300

    dynamic static path and props

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,17 @@ import Banner from "../components/banner";
 import styles from "../styles/Home.module.css";
 import useTrackLocation from "../hooks/use-track-location";
 import Card from "../components/card";
+import coffeeStoresData from "../data/coffee-stores.json";
 
-export default function Home() {
+export async function getStaticProps() {
+    return {
+        props: {
+            coffeeStores: coffeeStoresData,
+        }, // will be passed to the page component as props
+    };
+}
+
+export default function Home({ coffeeStores }) {
     const { handleTrackLocation, locationErrorMsg, isFindingLocation } = useTrackLocation();
     const handleOnBannerBtnClick = () => {
         handleTrackLocation();
@@ -26,12 +35,28 @@ export default function Home() {
                 />
                 <div className={styles.heroImage}>
                     <Image src="/static/hero-image.png" width={700} height={400} alt="hero image" />
-                    <Card
-                        name="cofee been"
-                        href="/static/background.png"
-                        imgUrl="/static/background.png"
-                    />
                 </div>
+
+                {coffeeStores.length > 0 && (
+                    <div className={styles.sectionWrapper}>
+                        <h2 className={styles.heading2}>Toronto stores</h2>
+                        <div className={styles.cardLayout}>
+                            {coffeeStores.map((coffeeStore) => {
+                                return (
+                                    <Card
+                                        key={coffeeStore.id}
+                                        name={coffeeStore.name}
+                                        imgUrl={
+                                            coffeeStore.imgUrl ||
+                                            "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                                        }
+                                        href={`/coffee-store/${coffeeStore.id}`}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
             </main>
         </div>
     );
