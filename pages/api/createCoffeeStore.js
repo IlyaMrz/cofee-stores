a9767e8c218a3d8333948fb6a181eed859cd2b9f
morozov ilya <knpkaa@gmail.com>
2021-11-19 20:33:46 +0300

    get clean fields json

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,6 +5,17 @@ const table = base("coffee-stores");
 
 console.log({ table });
 
+const getMinifiedRecord = (record) => {
+    return {
+        recordId: record.id,
+        ...record.fields,
+    };
+};
+
+const getMinifiedRecords = (records) => {
+    return records.map((record) => getMinifiedRecord(record));
+};
+
 export default async function createCoffeeStore(req, res) {
     const findCoffeeStoreRecords = await table
         .select({
@@ -15,7 +26,7 @@ export default async function createCoffeeStore(req, res) {
     console.log({ findCoffeeStoreRecords });
 
     if (findCoffeeStoreRecords.length !== 0) {
-        res.json(findCoffeeStoreRecords);
+        res.json(getMinifiedRecords(findCoffeeStoreRecords));
     } else {
         res.json({ m: "create a record" });
     }
